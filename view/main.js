763a2ace9e10c209e5885761435b5dcc785908c8
busyxin <xin@busyxin.com>
2019-11-16 22:54:40 +0200

    Add speed control and style + vibration feedbacks

--- view/main.js
+++ view/main.js
@@ -1,8 +1,15 @@
+var ROUND_TIME = 30;
+
 $(document).ready(function() {
-	var header	= document.querySelector('header');
+  var goFS = document.getElementById("goFS");
+  goFS.addEventListener("click", function() {
+      document.body.requestFullscreen();
+  }, false);
+
+	var turnAngle	= document.querySelector('.turn-angle');
 	var video  	= document.querySelector('video');
 	var canvas 	= document.querySelector('canvas');
-	var initAlpha, alpha, steeringAngle;
+	var initAlpha, alpha, steeringAngle, speedAngle;
 
 	window.addEventListener("deviceorientation", function(event) {
 		if (!video || !canvas) {
@@ -18,27 +25,39 @@ $(document).ready(function() {
 
 			steeringAngle = parseFloat(alpha) - parseFloat(initAlpha);
 
-			if (steeringAngle > 90) {
-
+			if (steeringAngle > 180) {
+				steeringAngle -= 180;
 				video.style.transform = "rotate(" + String(-steeringAngle) + "deg)";
 				canvas.style.transform = "rotate(" + String(-steeringAngle) + "deg)";
 				socket_steering_handler(steeringAngle);
-				header.textContent = steeringAngle;
+				turnAngle.textContent = steeringAngle;
 
 			} else {
 				video.style.transform = "rotate(" + String(steeringAngle) + "deg)";
 				canvas.style.transform = "rotate(" + String(steeringAngle) + "deg)";
 				socket_steering_handler(-steeringAngle);
-				header.textContent = -steeringAngle;
+				turnAngle.textContent = -steeringAngle;
 			}
 		}
 	});
 
+	$(".speed-control").on("input change", function(e) {
+		speedAngle = parseInt($(this).val()) * 0.9;
+		socket_speed_handler(speedAngle);
+		window.navigator.vibrate(Math.abs(speedAngle) * 10);
+	})
+
 	var time = 0;
 	var timebar = document.querySelector('.timebar-fill');
 
-	setInterval(function(){
-		timebar.style.width = (time/60)*100 + "%";
+	var timer = setInterval(function(){
+		timebar.style.width = (time/ROUND_TIME)*100 + "%";
 		time += 1;
+
+		if (time == ROUND_TIME) {
+			clearInterval(timer)
+			timebar.style.width = "100%";
+			window.navigator.vibrate(2000);
+		}
 	}, 1000);
 })

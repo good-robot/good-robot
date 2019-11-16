a91995841bdc3eb2f41b03113387eb265a59a179
Philip Claesson <philipcl@kth.se>
2019-11-17 00:16:22 +0200

    serial

--- webcam-dev/ser.py
+++ webcam-dev/ser.py
@@ -12,10 +12,12 @@ ser = serial.Serial(
     timeout=10
 )
 counter=0
-while 1:
+while counter < 10:
     angle = 90 * (counter % 2)
     print('steer %d'%(angle))
     ser.write('steer %d'%(angle))
     ser.write('steer %d\r'%(angle))
     time.sleep(1)
     counter += 1
+
+ser.close()

5cea5e8e0281293ebc725e8cd711bac517c16ffe
Ben Olayinka <ben.olayinka@gmail.com>
2019-11-17 06:18:06 +0200

    websocket fix

--- view/js/control.js
+++ view/js/control.js
@@ -60,59 +60,62 @@ function control_connect() {
 			console.log("Server sent his local time... " +message.server_time);
 		}
 	)
-} 
 
-//-----------------------------------------
-//	CLIENT->SERVER CONTROLS
-//-----------------------------------------
-//	Controls inside the webpage can emit async events to the server
-//	In this example I have a push button and I catch keyboard strokes
+	
+	//-----------------------------------------
+	//	CLIENT->SERVER CONTROLS
+	//-----------------------------------------
+	//	Controls inside the webpage can emit async events to the server
+	//	In this example I have a push button and I catch keyboard strokes
 
-/////////
-// LISTENERS FOR STEERING AND SPEED
-////////
-function socket_steering_handler(steeringAngle)
-{
-	// steeringAngle must be in [-90, 90]
-	mysocket.emit("steering", { payload: steeringAngle });
-	console.log("Steering angle ", steeringAngle);
-}
-function socket_speed_handler(speedAngle)
-{
-	// steeringAngle must be in [-90, 90]
-	mysocket.emit("speed", { payload: speedAngle });
-	console.log("Speed angle ", speedAngle);
-}
-
-/////////
-// LISTENERS FOR KEYSTROKE STEERING, SPEED AND STOP
-////////
-window.document.addEventListener
-(
-	"keypress",
-	function onEvent(event)
+	/////////
+	// LISTENERS FOR STEERING AND SPEED
+	////////
+	function socket_steering_handler(steeringAngle)
 	{
-		switch (event.key) {
-		case ' ':
-			mysocket.emit("stop");
-			break;
-		case 'w':
-			mysocket.emit("speed", { payload: 45 });
-			break;
-		case 's':
-			mysocket.emit("speed", { payload: -45 });
-			break;
-		case 'a':
-			mysocket.emit("steering", { payload: -90 });
-			break;
-		case 'd':
-			mysocket.emit("steering", { payload: 90 });
-			break;
-		default:
-			console.log('Sorry, use WASD to move and SPACE to stop.');
-		}
-		//Inform the server that a key has been pressed
-		// mysocket.emit("keyboard", { payload: event.key });
-		console.log("Key press..." + event.key);
+		// steeringAngle must be in [-90, 90]
+		mysocket.emit("steering", { payload: steeringAngle });
+		console.log("Steering angle ", steeringAngle);
 	}
-);
+	function socket_speed_handler(speedAngle)
+	{
+		// steeringAngle must be in [-90, 90]
+		mysocket.emit("speed", { payload: speedAngle });
+		console.log("Speed angle ", speedAngle);
+	}
+
+	/////////
+	// LISTENERS FOR KEYSTROKE STEERING, SPEED AND STOP
+	////////
+	window.document.addEventListener
+	(
+		"keypress",
+		function onEvent(event)
+		{
+			switch (event.key) {
+			case ' ':
+				mysocket.emit("stop");
+				break;
+			case 'w':
+				mysocket.emit("speed", { payload: 45 });
+				break;
+			case 's':
+				mysocket.emit("speed", { payload: -45 });
+				break;
+			case 'a':
+				mysocket.emit("steering", { payload: -90 });
+				break;
+			case 'd':
+				mysocket.emit("steering", { payload: 90 });
+				break;
+			default:
+				console.log('Sorry, use WASD to move and SPACE to stop.');
+			}
+			//Inform the server that a key has been pressed
+			// mysocket.emit("keyboard", { payload: event.key });
+			console.log("Key press..." + event.key);
+		}
+	);
+
+	
+} 
